7b43271d789d60a19c6a922def0dc6d471d2f5d3
ferdyhape <ferdyhahan5@gmail.com>
2023-01-17 01:25:09 +0700

    add popperjs and sweetalert to assets

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+function showCertificate() {
+    Swal.fire({
+        title: "<i>Title</i>",
+        html: "Testno  sporocilo za objekt: <b>test</b>",
+        confirmButtonText: "V <u>redu</u>",
+    });
+}
